db61bd479b302d5eec69fc8a3a5745abcbfc7f88
fabarca33 <fabarca629@gmail.com>
2023-10-01 22:12:09 -0400

    Commit FabianMusic with new models, controllers and views

---
+++ Week 5/FabianMusic/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FabianMusic.Models;
+
+namespace FabianMusic.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactModel model)
+        {
+            //ViewBag.FV = model.ContactModel();
+            return View(model);
+        }
+    }
+}
