3cc2329524fee060895168a658cb673974a6ff43
fabarca33 <fabarca629@gmail.com>
2023-11-03 14:07:43 -0400

    Initial commit Week 9

---
+++ Week 9/FabianMusic/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using FabianMusic.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FabianMusic.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
+        public ViewResult SetSessionVariables()
+            {
+                /*string fabian = HttpContext.Session.GetString("FirstName");
+                string abarca = HttpContext.Session.GetString("LastName");
+                string IT2030 = HttpContext.Session.GetString("Course");
+                int num = HttpContext.Session.GetInt32("FavNum");*/
+
+                HttpContext.Session.SetString("FirstName", "Fabian");
+                HttpContext.Session.SetString("LastName", "Abarca");
+                HttpContext.Session.SetString("Course", "IT2030");
+                HttpContext.Session.SetInt32("FavNum", 33);
+
+
+                return View();
+            }
+
+        public ViewResult ClearSessionVariables()
+            {
+                HttpContext.Session.Remove("FirstName");
+                HttpContext.Session.Remove("LastName");
+                HttpContext.Session.Remove("Course");
+                HttpContext.Session.Remove("FavNum");
+
+                return View();
+
+            }
+        public IActionResult Tools()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Week 9/FabianMusic/Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//must be called before AddControllersWithViews()
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// must be called before routes are mapped
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(60 * 5);
+    options.Cookie.IsEssential = true;
+});
