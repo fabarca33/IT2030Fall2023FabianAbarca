5ca452209037bea9bb4a605ebdad8ad000d17d81
fabarca33 <fabarca629@gmail.com>
2023-11-15 09:27:32 -0500

    Commit Lab Week 11

---
+++ Week 11/FabianMusic/Models/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FabianMusic.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Please enter your the product's name.")]
+        public string ProductName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please write a description.")]
+        public string ProductDescription { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please choose a picture.")]
+        public string ProductImage { get; set; } = string.Empty;
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal ProductPrice { get; set; }
+    }
+}
